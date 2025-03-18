6a2140d016866f6ec9135e7381cba65d404d1f50
fndsz <45010248+fndsz@users.noreply.github.com>
2025-03-18 10:53:06 +0800

    Update fndsz.sh

--- fndsz.sh
+++ fndsz.sh
@@ -23,6 +23,7 @@ sed -i '$a src-git immortalwrt https://github.com/immortalwrt/packages' feeds.co
 
 ####### MMDVM
 git clone https://github.com/fndsz/mmdvm-op.git package/mmdvm/mmdvm-openwrt
+git clone https://github.com/fndsz/mmdvmpatch.git package/mmdvm/mmdvm-openwrt
 
 ####### wolplus
 git clone https://github.com/siwind/luci-app-wolplus.git package/lean/luci-app-wolplus 
