1ba4d208bd4c1389e034a68b6bc4cbee862c05e4
fndsz <45010248+fndsz@users.noreply.github.com>
2025-03-23 12:07:55 +0800

    Update fndsz.sh

--- fndsz.sh
+++ fndsz.sh
@@ -14,7 +14,7 @@
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
-sed -i '$a src-git immortalwrt https://github.com/immortalwrt/packages' feeds.conf.default
+#sed -i '$a src-git immortalwrt https://github.com/immortalwrt/packages' feeds.conf.default
 
 ####### MMDVM
 git clone https://github.com/fndsz/mmdvm-op.git package/mmdvm/mmdvm-openwrt
